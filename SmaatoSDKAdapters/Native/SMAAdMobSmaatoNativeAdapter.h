8f4ddc004c39158c9288dc8ef082d432e2be4c9e
ashwinee-smaato <ashwinee.mhaske@smaato.com>
2025-04-16 11:38:26 +0000

    11.3.0.0

---
+++ SmaatoSDKAdapters/Native/SMAAdMobSmaatoNativeAdapter.h
@@ -0,0 +1,12 @@
+//
+//  SMAAdMobSmaatoNativeAdapter.h
+//  SmaatoSDKAdmobNativeAdapter
+//
+//
+
+#import <Foundation/Foundation.h>
+#import <GoogleMobileAds/GoogleMobileAds.h>
+
+@interface SMAAdMobSmaatoNativeAdapter: NSObject
+@property (class, nonatomic, readonly) NSString *version;
+@end
