7a02da437a37e574386eb6611a7541f2c4a4446b
ashwinee-smaato <ashwinee.mhaske@smaato.com>
2025-04-18 15:28:04 +0000

    11.3.0.0

---
+++ SmaatoSDKAdapters/Interstitial/SMAAdMobSmaatoInterstitialAdapter.h
@@ -0,0 +1,11 @@
+//
+//  SMAAdMobSmaatoInterstitialAdapter.h
+//  SmaatoSDKAdmobInterstitialAdapter
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SMAAdMobSmaatoInterstitialAdapter: NSObject
+@property (class, nonatomic, readonly) NSString *version;
+@end
---
+++ SmaatoSDKAdapters/RewardedVideo/SMAAdMobSmaatoRewardedVideoAdapter.h
@@ -0,0 +1,12 @@
+//
+//  SMAAdMobSmaatoRewardedVideoAdapter.h
+//  SmaatoSDKAdmobRewardedVideoAdapter
+//
+//
+
+#import <Foundation/Foundation.h>
+#import <GoogleMobileAds/GoogleMobileAds.h>
+
+@interface SMAAdMobSmaatoRewardedVideoAdapter: NSObject <GADMediationAdapter>
+@property (class, nonatomic, readonly) NSString *version;
+@end
